12db07ac4ad3b49be6964e0ed13da66c81f4c92a
Stephane <stephane.conreaux@yahoo.fr>
2021-05-12 13:20:06 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
+
+export class AppClass extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			text: 'Hello'
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<p>{this.state.text}</p>
+					<button
+						onClick={() => {
+							this.setState({ text: 'Bye' });
+						}}
+					>
+						Change Text
+					</button>
+				</header>
+			</div>
+		);
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ monsters, setMonsters ] = React.useState([]);
+	const [ searchField, setSearchField ] = React.useState('');
+
+	// https://jsonplaceholder.typicode.com/users
+	React.useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((users) => setMonsters(users));
+	}, []); // componentDidMount
+
+	if (!monsters) {
+		return <div>Loading...</div>;
+	}
+
+	const filteredMonsters =
+		searchField === ''
+			? monsters
+			: monsters.filter((m) => m.name.toLowerCase().includes(searchField.toLowerCase()));
+
+	const handleChange = (e) => {
+		setSearchField(e.target.value);
+	};
+
+	return (
+		<div className="App">
+			<h1>Monsters Rolodex</h1>
+			<SearchBox handleChange={handleChange} placeholder="search monsters" />
+			<CardList monsters={filteredMonsters} />
+		</div>
+	);
 }
 
 export default App;
